7b68fdc1d04b24d4fe17d0527477622bdc8c68d5
Prashant Mishra <95920112+prashy19@users.noreply.github.com>
2022-05-29 14:34:04 +0530

    Update creation.c

--- creation.c
+++ creation.c
@@ -1,7 +1,6 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-
 /*
     A linked list consists of various nodes, and each node contains two things: 
     One is the data, and the other is the pointer, which will point to the next node.
@@ -16,10 +15,12 @@
 
 struct Node
 {
-    int data;           // variable 'data' will store the data elements.
-    struct Node *next;      // pointer 'next' is of a 'structure Node' data type and 
-                            // will hold the address of its next node, that will pointing onto the next node.
-}*first = NULL;         // declared pointer 'first' and first node of Linked List is intialized as NULL.
+    int data;          	 	    // variable 'data' will store the data elements.
+
+    struct Node *next;     		// pointer 'next' is of a 'structure Node' data type and will hold the
+                           		// address of its next node, that will be pointing onto the next node.
+
+}*first = NULL;         	    // declared pointer 'first' and first node of Linked List is initialized as NULL.
 
 
 // function to create a Linked List.
@@ -28,28 +29,31 @@ void create(int arr[], int n)
 {
     // pointer 'temp' will help in creation of new nodes,
     // and pointer 'last' will add a new node at the end of the Linked List.
+
     struct Node *temp, *last;
     
 
-    // dynamically allocationg the memory for the first node inside Heap.
+    // dynamically allocating the memory for the first node inside Heap.
 
     first = (struct Node *)malloc(sizeof(struct Node));
-    first -> data = arr[0];         // assigned first elemnt of Array in first node of Linked List.
-    first -> next = NULL;       // linked list with a single node.
-    last = first;           // pointer 'last' pointing to the first node.
+    first -> data = arr[0];         	// assigned first element of Array in first node of Linked List.
+    first -> next = NULL;       		// linked list with a single node.
+    last = first;           			// pointer 'last' pointing to the first node.
 
 
     // loop for traversing the Array elements and will create the new nodes of the Linked List.
     
     for (int i = 1; i < n; i++)
     {
-        temp = (struct Node *)malloc(sizeof(struct Node));  // allocating memory for the new nodes inside Heap.
+        // allocating memory for the new nodes inside Heap.
+
+        temp = (struct Node *)malloc(sizeof(struct Node));  		
         
         temp -> data = arr[i];          // assign  the Array elements in Linked List one-by-one.
                                         // arr[1] , arr[2] , arr[3] , arr[4] , arr[5]
         
         temp -> next = NULL;            // newly created node next's contains NULL.
-                                        // arr[1] | _____ , arr[2] | _____ , arr[3] | _____ , arr[4] | _____ , arr[5] | NULL
+                                        // arr[1] | ____ , arr[2] | ____ , arr[3] | ____ , arr[4] | ____ , arr[5] | NULL
                                         // blank spaces referring the address of the 'next node'.
 
         last -> next = temp;            // pointer 'last' will now point onto the temp node's next.
@@ -65,29 +69,31 @@ void create(int arr[], int n)
 
 void Display(struct Node *newnode)
 {
-    while (newnode != NULL)         // if new node is not NULL and contains the address of next node, then enter the loop.
+while (newnode != NULL)         	// if new node is not NULL and contains the address of next node, then enter the loop.
     {
-        printf("%d\t", newnode -> data);        // print the data.
-        newnode = newnode -> next;          // move new node to next node.
-    }                                       // exit the loop.
+        printf("%d\t", newnode -> data);        	// print the data.
+        newnode = newnode -> next;         			// move new node to next node.
+    }                                      			// exit the loop.
 }
 
 
 // Recursive function for traversing the Linked List.
 /*
-    lets assume a Linked List example for better understanding of Recursive funtion
+    let’s assume a Linked List example for better understanding of Recursive function
 
     | 100 | ---> | 4|120 | ---> | 6|150 | ---> | 9|200 | ---> | 12|260 | ---> | 42|300 | ---> | 16|NULL | 
+    address     data|address
+    of next         |of next
 
     execution:
 
         newnode(first) = 100 != NULL            (enter the loop)
         
-        Step-1: print: newnode's data that is = 4
+        Step-1: print: newnode's data that is =  4
                 
                 [now, function 'recDisplay' will call itself]
 
-        Step-2: call: newnode's next that is = 120 
+        Step-2: call: newnode's next that is =  120 
                 
                 [now, it will repeat step-1 and step-2 until newnode's next becomes NULL]
 
@@ -106,7 +112,7 @@ void Display(struct Node *newnode)
         Step-1:    print =  16
         Step-2:    call =   0
 
-        newnode = 0(NULL)
+        newnode =  0(NULL)
                 [exit the loop and the call will get terminated]
 */
 
@@ -125,15 +131,18 @@ void recDisplay(struct Node *newnode)
 
 int main()
 {
-    int arr[] = {4, 6, 9, 12, 42, 16};          // declared and intialized an Array of size '6' 
+    int arr[] = {4, 6, 9, 12, 42, 16};          // declared and initialized an Array of size '6' 
 
-    create(arr, 6);         // calling the create function and passing the Array along with its size as a parameter.
+    create(arr, 6);         	// calling the create function and passing the Array along with its size as a parameter.
 
-    Display(first);         // calling the Display function and passing the 'first' as a pointer,
-                            // the pointer 'first' will become the pointer 'newnode' in Display function.
-    printf("\n");
-
-    recDisplay(first);      // calling the recursive function and passing the 'first' as a pointer.
-
+    printf("the data elements of Linked List are: \n");
+    
+    Display(first);         	 // calling the Display function and passing the 'first' as a pointer,
+                                 // the pointer 'first' will become the pointer 'newnode' in Display function.
+    
+    printf("\n displaying the elements using Recursion: \n");
+   
+    recDisplay(first);     	    // calling the recursive function and passing the 'first' as a pointer.
+    
     return 0;
 }
